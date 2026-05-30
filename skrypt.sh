512552a41bc00bd94429ba76f21a502be8141b66
Cingomark <o.zieba2004@gmail.com>
2026-05-30 12:00:56 +0200

    cz4

--- skrypt.sh
+++ skrypt.sh
@@ -7,7 +7,8 @@ case "$1" in
         date
         ;;
     --logs)
-        for i in {1..100}; do
+        count=${2:-100}
+        for ((i=1; i<=count; i++)); do
             filename="log${i}.txt"
             echo "Nazwa pliku: $filename" > "$filename"
             echo "Skrypt: $SCRIPT_NAME" >> "$filename"
